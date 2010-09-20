53400a08b6d1774c996857a0b0f10b7b9d2c79ec
dsmathers <drew.smathers@transgaming.com>
2010-09-20 15:56:25 -0400

    added setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from txbeatlounge import version as VERSION
+
+PKGNAME = 'txbeatlounge'
+
+def load_requirements(requirements):
+    fd = open(requirements)
+    entries = []
+    for line in fd:
+        entry = line.strip()
+        if not entry:
+            continue
+        if entry[0] in ('-', '#'):
+            continue
+        dep, v = entry.split('==')
+        entries.append(entry)
+    fd.close()
+    return entries
+
+requirements = load_requirements('requirements.txt')
+packages = [PKGNAME] + [ ( '%s.%s' % (PKGNAME, pkg) ) for pkg in find_packages(PKGNAME) ]
+
+setup(
+    name='Twisted Beat Lounge',
+    author = "Beatscape Drone Unicorns",
+    version=VERSION,
+    description='Twisted Beat Lounge is a framework for laying beats with fluidsynth',
+    packages=packages,
+    install_requires = requirements,
+    include_package_data=True,
+    zip_safe=False,
+)
+
