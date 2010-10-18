0618261c5420af520c38057d1ac9dc0d8fb11509
dsmathers <drew.smathers@transgaming.com>
2010-10-18 19:52:13 -0400

    player module

---
+++ txbeatlounge/experiment/player.py
@@ -0,0 +1,93 @@
+from zope.interface import Interface, Attribute, implements
+
+
+__all__ = [ 'IPlayer', 'INotePlayer', 'IChordPlayer',
+            'BasePlayer', 'NotePlayer', 'ChordPlayer',
+            'N', 'F', 'Num', 'generateSounds' ]
+
+
+class IPlayer(Interface):
+    instr = Attribute('Instrument that provides playnote(note, velocity)')
+    velocity = Attribute('IFilter to get current velocity')
+    stop = Attribute('Callable to get stop time on current note/chord, or None for non-stop')
+
+class INotePlayer(Interface):
+    note = Attribute('Callable to get the current note to play')
+
+class IChordPlayer(Interface):
+    chord = Attribute('Callable to get the current chord to play')   
+ 
+
+
+class BasePlayer(object):
+    implements(IPlayer)
+
+    def __init__(self, instr, velocity, stop, clock=None):
+        self.instr = instr
+        self.velocity = velocity
+        self.stop = stop
+        if clock is None:
+            from txbeatlounge.scheduler2 import clock
+        self.clock = clock
+
+    def play(self):
+        v, o = self.velocity(110, 110)
+        n = self._next()()
+        if n is None:
+            return
+        self._on_method(n, v)
+        stop = self.stop()
+        if stop is not None:
+            self.clock.callLater(stop, self._off_method, n)
+
+
+class NotePlayer(BasePlayer):
+    implements(INotePlayer)
+
+    def __init__(self, instr, note, velocity, stop=lambda : None, clock=None):
+        super(NotePlayer, self).__init__(instr, velocity, stop, clock)
+        self.note = note
+        self._on_method = self.instr.playnote
+        self._off_method = self.instr.stopnote
+
+    def _next(self):
+        return self.note
+
+class ChordPlayer(BasePlayer):
+    implements(IChordPlayer)
+    
+    def __init__(self, instr, note, velocity, stop=lambda : None, clock=None):
+        super(ChordPlayer, self).__init__(instr, velocity, stop, clock)
+        self.chord = chord
+        self._on_method = self.instr.playchord
+        self._off_method = self.instr.stopchord
+
+    def _next(self):
+        return self.chord
+
+def generateSounds(g):
+    return lambda : g.next()()
+
+
+def N():
+    return None
+
+def F(numerator, denominator):
+    _result = numerator / float(denominator)
+    def f():
+        return _result
+    return f
+
+def Num(n):
+    def f():
+        return n
+    return f
+
+def _numberwang():
+    g = globals()
+    for i in range(128):
+        g['n%s' % i] = Num(i)
+        __all__.append('n%s' % i)
+_numberwang()
+
+
