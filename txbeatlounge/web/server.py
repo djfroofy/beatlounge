2158b5e3aedc6b0659cad6f16c6abf628dc8529a
Skylar Saveland <skylar.saveland@gmail.com>
2011-01-03 18:23:20 -0500

    server has some broken stuff but the pp(help(complib)) is off the chain with the css for rilz

--- txbeatlounge/web/server.py
+++ txbeatlounge/web/server.py
@@ -4,7 +4,7 @@
 built on https://github.com/rlotun/txWebSocket
 """
 
-import StringIO, sys
+import StringIO, sys, time
 from datetime import datetime
 
 from twisted.internet.protocol import Protocol, Factory
@@ -16,17 +16,29 @@ from twisted.conch.manhole import ManholeInterpreter
 
 from websocket import WebSocketHandler, WebSocketSite
 
+import pprint
+pp = pprint.PrettyPrinter(indent=2)
+pp = pp.pprint
+
 
 class CodeHandler(WebSocketHandler):
 
+    special_messages_to_method = {'restart': 'restart'}
+
     def __init__(self, transport):
         WebSocketHandler.__init__(self, transport)
+        self.startInterpreter()
+
+    def startInterpreter(self):
         self.interpreter = ManholeInterpreter(self)
         self.interpreter.runcode("""
 from txbeatlounge.scheduler import clock as reactor
 reactor.synthAudioDevice = "coreaudio"
 reactor.run()
 #from comps.core import *
+import pprint
+pp = pprint.PrettyPrinter(indent=2)
+pp = pp.pprint
 """)
 
     def __del__(self):
@@ -34,8 +46,15 @@ reactor.run()
 
     def frameReceived(self, frame):
         """ """
-        print 'Peer: ', self.transport.getPeer()
-        self.interpreter.runcode(frame)
+        #print 'Peer: ', self.transport.getPeer()
+
+        if not self.interpreter:
+            self.startInterpreter()
+
+        if frame in self.special_messages_to_method.keys():
+            getattr(self, self.special_messages_to_method[frame])()
+        else:
+            self.interpreter.runcode(frame)
 
     def addOutput(self, data, async=False):
         self.transport.write(data)
@@ -47,6 +66,23 @@ reactor.run()
     def connectionLost(self, reason):
         print 'Lost connection.', reason
         reason.printTraceback()
+
+    def restart(self):
+        """Method that doesn't work @drew TODO :)
+        """
+        self.interpreter.runcode("reactor.task.stop()")
+        self.interpreter.runcode("""
+from txbeatlounge.instrument.fsynth import ChordPlayerMixin
+l = locals()
+for v in l.values():
+    if isinstance(v, ChordPlayerMixin):
+        v.stopall()
+
+""")
+        self.interpreter = None
+        #time.sleep(3)
+        #self.startInterpreter()
+
 
 
 if __name__ == "__main__":
