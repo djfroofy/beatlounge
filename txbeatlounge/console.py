b15d398def9438e2f38b6073c67633eca7a70262
Drew Smathers <drew.smathers@gmail.com>
2011-01-05 05:32:51 -0500

    PersistenConsoleManhole - restor console history from ~/.beatlounge.history

--- txbeatlounge/console.py
+++ txbeatlounge/console.py
@@ -4,10 +4,46 @@ import os, tty, sys, termios
 
 from twisted.internet import stdio, protocol, defer
 from twisted.conch.stdio import ServerProtocol, ConsoleManhole
+from twisted.conch.recvline import HistoricRecvLine
 from twisted.python import failure, reflect, log
+from twisted.python.filepath import FilePath
 
 from txbeatlounge.scheduler import clock as reactor
 
+class PersistentConsoleManhole(ConsoleManhole):
+
+    historyFile = os.path.join(os.environ.get('HOME', ''), '.beatlounge.history')
+    maxLines = 2**12
+
+    def connectionMade(self):
+        self._historySession = os.getpid()
+        self._historyFd = open(self.historyFile + ('.%d' % self._historySession), 'w')
+        ConsoleManhole.connectionMade(self)
+        if os.path.exists(self.historyFile):
+            with open(self.historyFile) as fd:
+                lineCount = 0
+                for line in fd:
+                    if not line.strip(): continue
+                    if lineCount > self.maxLines:
+                        self.historyLines.pop(0)
+                    self.historyLines.append(line[:-1])
+                    lineCount += 1
+        for line in self.historyLines:
+            self._historyFd.write(line + '\n')
+        self.historyPosition = len(self.historyLines)
+
+    def connectionLost(self, reason):
+        self._historyFd.close()
+        path = FilePath(self.historyFile + ('.%d' % self._historySession))
+        path.moveTo(FilePath(self.historyFile))
+        ConsoleManhole.connectionLost(self, reason)
+
+    def handle_RETURN(self):
+        line = ''.join(self.lineBuffer)
+        rv = ConsoleManhole.handle_RETURN(self)
+        if line:
+            self._historyFd.write(line + '\n')
+        return rv
 
 def runWithProtocol(klass, audioDev):
     fd = sys.__stdin__.fileno()
@@ -36,7 +72,7 @@ def main(argv=None, reactor=None):
     if argv:
         klass = reflect.namedClass(argv[0])
     else:
-        klass = ConsoleManhole
+        klass = PersistentConsoleManhole
     log.msg('audio dev: %s' % audioDev)
     runWithProtocol(klass, audioDev)
 
