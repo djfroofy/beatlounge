7b9648c9d83ca6c3116b73c8bed7b9e67e4fb316
Drew Smathers <drew.smathers@gmail.com>
2011-01-12 14:54:36 +0800

    some tweaks to web.server: start clock in main rather than on connection, update interpreter namespace with console.consoleNamespace, convert print stmts to log.msg

--- txbeatlounge/web/server.py
+++ txbeatlounge/web/server.py
@@ -7,6 +7,7 @@ built on https://github.com/rlotun/txWebSocket
 import argparse
 import StringIO, sys, time
 from datetime import datetime
+import pprint
 
 from twisted.internet.protocol import Protocol, Factory
 from twisted.python import log
@@ -17,10 +18,7 @@ from twisted.conch.manhole import ManholeInterpreter
 
 from websocket import WebSocketHandler, WebSocketSite
 
-import pprint
-pp = pprint.PrettyPrinter(indent=2)
-pp = pp.pprint
-
+from txbeatlounge.console import consoleNamespace
 
 class CodeHandler(WebSocketHandler):
 
@@ -31,19 +29,15 @@ class CodeHandler(WebSocketHandler):
         self.startInterpreter()
 
     def startInterpreter(self):
+        log.msg('starting interpreter')
         self.interpreter = ManholeInterpreter(self)
-        self.interpreter.runcode("""
-from txbeatlounge.scheduler import clock as reactor
-reactor.synthAudioDevice = "coreaudio"
-reactor.run()
-#from comps.core import *
-import pprint
-pp = pprint.PrettyPrinter(indent=2)
-pp = pp.pprint
-""")
+        self.interpreter.locals = dict(consoleNamespace)
+        pp = pprint.PrettyPrinter(indent=2)
+        pp = pp.pprint
+        self.interpreter.locals['pp'] = pp
 
     def __del__(self):
-        print 'Deleting handler'
+        log.msg('Deleting handler')
 
     def frameReceived(self, frame):
         """ """
@@ -62,45 +56,59 @@ pp = pp.pprint
         #log.msg(data)
 
     def connectionMade(self):
-        print 'Connected to client.'
+        log.msg('Connected to client.')
 
     def connectionLost(self, reason):
-        print 'Lost connection.', reason
-        reason.printTraceback()
+        log.msg('Lost connection.')
+        log.err(reason)
 
     def restart(self):
-        """Method that doesn't work @drew TODO :)
         """
-        self.interpreter.runcode("reactor.task.stop()")
-        self.interpreter.runcode("""
-from txbeatlounge.instrument.fsynth import ChordPlayerMixin
-l = locals()
-for v in l.values():
-    if isinstance(v, ChordPlayerMixin):
-        v.stopall()
+        Method that doesn't work @drew TODO :)
 
-""")
-        self.interpreter = None
+        I'm working on it ... sort of (in my head now) - but i think the right thing to
+        do is have some global registry of players and scheduled events and then stop
+        everything in the registry - players, then scheduled events for plain tasks. (drew)
+        """
+
+        # NOTE
+        # please don't alias clock as "reactor" anymore - i'm sorry i did
+        # that before - eventually BeatClock probably shouldn't even inherit
+        # from SelectReactor etc. and this get's confusing because it's hard to
+        # tell one from the other in code - they're not the same (e.g. Twisted's
+        # global singleton reactor and our custom reactor - BeatClock)
+
+        #self.interpreter.runcode("reactor.task.stop()")
+        #self.interpreter.runcode("""
+#from txbeatlounge.instrument.fsynth import ChordPlayerMixin
+#l = locals()
+#for v in l.values():
+#    if isinstance(v, ChordPlayerMixin):
+#        v.stopall()
+#
+#""")
+#        self.interpreter = None
         #time.sleep(3)
         #self.startInterpreter()
 
 
 
 if __name__ == "__main__":
-
+    from twisted.internet import reactor
+    from txbeatlounge.scheduler import clock
+    
     # Note, the port will need to be changed in the index.html
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', default=8080)
     args = parser.parse_args()
     port = int(args.p)
 
-
-    from twisted.internet import reactor
     log.startLogging(sys.stdout)
 
     root = File('.')
     site = WebSocketSite(root)
     site.addHandler('/code', CodeHandler)
     reactor.listenTCP(port, site, interface='127.0.0.1')
+    reactor.callWhenRunning(clock.run)
     reactor.run()
 
