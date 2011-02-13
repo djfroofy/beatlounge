8f125ab3ee8c0436e7f0a88dbc30ccad2559efa6
Drew Smathers <drew.smathers@gmail.com>
2011-02-13 17:35:19 -0500

    added PyoServer class with class methods for starting/stopping etc a pro Server; start() is idempodent which is particularly convenient

--- txbeatlounge/dsp.py
+++ txbeatlounge/dsp.py
@@ -3,11 +3,84 @@ from pyo import *
 #from twisted.python import log
 
 
-def startPYO():
-    s = Server(audio='jack').boot()
-    s.start()
-    return s
+class PyoServer:
+    _started = False
+    _server = None
+    _error_msg = 'Cannot %s server; server must be started first'
 
+    @classmethod
+    def start(cls, audio='jack', **kw):
+        """
+        Start a pyo server or do nothing if a server has already been started.
+
+        audio: the audio device (default: jack)
+        kw: additional keyword arguments to pyo.Server
+        """
+        if cls._server:
+            return
+        cls._server = s = Server(audio=audio, **kw)
+        s.boot()
+        s.start()
+        return s
+
+    @classmethod
+    def restart(cls):
+        """
+        Start and top the already running pyo Server.
+    
+        raises AssertionError if no server is running
+        """
+        assert cls._server, cls._error_msg % 'restart'
+        cls._server.stop()
+        cls._server.start()
+
+    @classmethod
+    def stop(cls):
+        """
+        Stop the running pyo Server.
+
+        raises AssertionError if no server is running
+        """
+        assert cls._server, cls._error_msg % 'stop'
+        cls._server.stop()
+
+    @classmethod
+    def shutdown(cls):
+        """
+        Shutdown the running pyo Server.
+
+        raises AssertionError if no server is running
+        """
+        assert cls._server, cls._error_msg % 'shutdown'
+        cls._server.stop()
+        cls._server.shutdown()
+
+    @classmethod
+    def reboot(cls):
+        """
+        Reboot the running pyo Server.
+
+        raises AssertionError if no server is running
+        """
+        assert cls._server, cls._error_msg % 'reboot'
+        cls._server.stop()
+        cls._server.shutdown()
+        cls._server.boot()
+        cls._server.start()
+
+startPyo = PyoServer.start
+stopPyo = PyoServer.stop
+shutdownPyo = PyoServer.shutdown
+restartPyo = PyoServer.restart
+rebootPyo = PyoServer.reboot
+
+# backwards compat - please use startPyo
+startPYO = startPyo
+
+# TODO
+# CHANGE REQUEST
+# maybe get rid of this. pyo doesn't let you have more than one server per process.
+# hence, stopPyo or shutdownPro are more convenient. 
 def stop(s):
     s.stop()
     s.shutdown()
