7d101a14391de6ed5a9be11a53476f57249ab11a
Drew Smathers <drew.smathers@gmail.com>
2011-04-17 16:55:50 -0400

    some documentation in txbeatlounge.midi

--- txbeatlounge/midi.py
+++ txbeatlounge/midi.py
@@ -11,6 +11,9 @@ __all__ = ['init', 'initialize', 'getInput', 'getOutput', 'printDeviceSummary',
            'NoteOnOffHandler' ]
 
 class PypmWrapper:
+    """
+    Simple wrapper around pypm calls which caches inputs and outputs.
+    """
 
     initialized = False
     inputs = []
@@ -22,6 +25,9 @@ class PypmWrapper:
 
     @classmethod
     def initialize(cls):
+        """
+        Initialize pypm if not already initialized.
+        """
         if cls.initialized:
             return
         pypm.Initialize()
@@ -47,6 +53,11 @@ class PypmWrapper:
 
     @classmethod
     def getInput(cls, dev):
+        """
+        Get an input with devive number 'dev' - dev may also be string matching
+        the target device. If the input was previously loaded this will return
+        the cached device.
+        """
         no = dev
         if isinstance(dev, basestring):
             no = cls.deviceMap[dev]['input']
@@ -58,6 +69,11 @@ class PypmWrapper:
 
     @classmethod
     def getOutput(cls, dev):
+        """
+        Get an output with devive number 'dev' - dev may also be string matching
+        the target device. If the output was previously loaded this will return
+        the cached device.
+        """
         no = dev
         if isinstance(dev, basestring):
             no = cls.deviceMap[dev]['output']
@@ -68,6 +84,9 @@ class PypmWrapper:
 
     @classmethod
     def printDeviceSummary(cls, printer=None):
+        """
+        Print device summary - inputs followed by outputs.
+        """
         if printer is None:
             def printer(line):
                 print line
@@ -123,6 +142,18 @@ a('SYSTEMRESET', 2, 0xFF)
 del a, g
 
 class MidiDispatcher(object):
+    """
+    Dispatcher for events received from a midi input channel.
+
+    Example usage:
+
+        init()
+        input = getInput(3)
+        def debug_event(event):
+            print event
+        disp = MidiDispatcher(input, [debug_event, NoteOnOffHandler(instr)])
+        disp.start()
+    """
 
     def __init__(self, midiInput, handlers, clock=None):
         self.clock = getClock(clock)
@@ -130,9 +161,17 @@ class MidiDispatcher(object):
         self.handlers = handlers
 
     def start(self):
+        """
+        Start the MidiDispatcher - this will schedule an event to call
+        all it's handlers every tick with any buffered events.
+        """
         self._event = self.clock.schedule(self).startLater(0, 1/96.)
 
     def __call__(self):
+        """
+        Call all our handlers with buffered events (max of 32 per call
+        are processed).
+        """
         for message in self.midiInput.Read(32):
             for call in self.handlers:
                 call(message)
@@ -141,6 +180,14 @@ class MidiDispatcher(object):
 class MidiHandler(object):
 
     def __call__(self, message):
+        """
+        Parse method and call method on self based on midi function. For example
+        if function is NOTEON_CHAN1, this will call our method noteon(), etc. If
+        a message has a channel as part of it's function, this will be the first
+        argument. After the first optional channel argument, remaining positional
+        arguments are passed to the method in the same order as specified in MIDI.
+        Not all MIDI functions need to be supplied or implemented in a subclass.
+        """
         packet, timestamp = message
         func, arg1, arg2, _pad = packet
         args = [ arg1, arg2 ][:FUNCTION_ARITY.get(func, 0)]
@@ -175,25 +222,54 @@ class _DummyInstrument:
 
 
 class NoteOnOffHandler(MidiHandler):
+    """
+    A simple MidiHandler which takes a mapping of channels to instruments.
+    """
 
     def __init__(self, instrs):
         self.instrs = instrs
 
     def noteon(self, channel, note, velocity, timestamp):
+        """
+        Immediately play instrument at channel with given note and velocity.
+        The timestamp is ignored. This is a noop if no instrument is mapped
+        to the given channel.
+        """
         self.instrs.get(channel, _DummyInstrument).playnote(note, velocity)
 
     def noteoff(self, channel, note, velocity, timestamp):
+        """
+        Immediately stop instrument at channel with given note. The velocity
+        and timestamp arguments are ignored. This is a noop if no instrument
+        is mapped to the given channel.
+        """
         self.instrs.get(channel, _DummyInstrument).stopnote(note)
 
 
 class ChordHandler(MidiHandler):
+    """
+    A chord handler is a simple MidiHandler which recognizes chords and sends
+    to its callback.
+
+    todo: Currently this implementation doesn't care about channels; but this
+    behavior should likely change in the near future.
+    """
 
     def __init__(self, callback, sustain=False):
+        """
+        callback: handler to receive chords
+        sustain: if True, only call our callback with noteon events
+        """
         self.callback = callback
         self.sustain = sustain
         self._chord = []
 
     def noteon(self, channel, note, velocity, timestamp):
+        """
+        Add note to chord and call our callback with updated chord.
+
+        Note that channel, velocity and timestamp arguments are ignored.
+        """
         debug('noteon channel=%s note=%s velocity=%s t=%s' % (channel, note, velocity, timestamp))
         if note not in self._chord:
             self._chord.append(note)
@@ -201,6 +277,12 @@ class ChordHandler(MidiHandler):
             self.callback(list(self._chord))
 
     def noteoff(self, channel, note, velocity, timestamp):
+        """
+        Remove note from chord. If the attribute `sustain` is `True` then we do not
+        call callback with the updated chord.
+
+        Note that channel, velocity and timestamp arguments are ignored.
+        """
         debug('noteoff channel=%s note=%s velocity=%s t=%s' % (channel, note, velocity, timestamp))
         if note in self._chord:
             self._chord.remove(note)
@@ -210,6 +292,10 @@ class ChordHandler(MidiHandler):
 
 
 class ClockSender(object):
+    """
+    A simple midi beat clock sender which can be used to synchronize external
+    MIDI devices.
+    """
 
     def __init__(self, midiOut, clock=None):
         self.clock = getClock(clock)
@@ -217,6 +303,12 @@ class ClockSender(object):
         self._started = False
 
     def start(self):
+        """
+        Start the ClockSender - on the next measure begin sending MIDI beat
+        clock events.  The first run will send a START and TIMINGCLOCK event.
+        Subsequent calls (24 per quarter note), will send bare TIMINGCLOCK
+        events.
+        """
         self._event = self.clock.schedule(self).startLater(1, 1/96.)
 
     def __call__(self):
