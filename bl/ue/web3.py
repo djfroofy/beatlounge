2cae9ad9969abfdbd8a693ff3066e76b46241397
Skylar Saveland <skylar.saveland@gmail.com>
2011-05-07 15:54:36 -0400

    cc-web3.html and stuff

--- bl/ue/web3.py
+++ bl/ue/web3.py
@@ -7,8 +7,6 @@ from twisted.web.static import File
 from websocket import WebSocketHandler, WebSocketSite
 
 
-#class BaseHandler(WebSocketHandler):
-
 class BaseHandler(WebSocketHandler):
 
     values = []
@@ -33,6 +31,8 @@ class BaseHandler(WebSocketHandler):
 
 
 class ListHandler(BaseHandler):
+    values = []
+    transports = []
 
     def frameReceived(self, frame):
         print 'frame:', frame
@@ -48,6 +48,8 @@ class ListHandler(BaseHandler):
 
 
 class ArpHandler(ListHandler):
+    values = []
+    transports = []
     arps = []
 
     def frameReceived(self, frame):
@@ -56,39 +58,51 @@ class ArpHandler(ListHandler):
             a.reset(self.values)
 
 
-
-
-
-class SliderHandler(WebSocketHandler):
+class ValuesHandler(BaseHandler):
+    """keeps a dict in stead of a list as ListHandler"""
 
     values = {}
     transports = []
 
-    def __init__(self, transport):
-        print '__init__', transport
-        WebSocketHandler.__init__(self, transport)
+    def connectionMade(self):
+        BaseHandler.connectionMade(self)
 
-    def __del__(self):
-        print 'Deleting handler'
-        self.transports.remove(self.transport)
+        if self.values:
+            self.transport.write(json.dumps(self.values))
+
 
     def frameReceived(self, frame):
         """value should be as <slider-num>-<value>"""
-        index, value = [int(n) for n in frame.split('-')]
+
+        print 'ValuesHandler.frameReceived: ', frame
+        index, value = frame.split('-')
         self.values[index] = value
-        print 'value:', frame
-        print self.values
         for t in self.transports:
             t.write(json.dumps(self.values))
 
-    def connectionMade(self):
-        print 'Connected to client.'
-        self.transports.append(self.transport)
-        self.transport.write(self.values)
 
-    def connectionLost(self, reason):
-        print 'Lost connection.', reason
-        self.transports.remove(self.transport)
+class ControlChangeHandler(ValuesHandler):
+    """
+    values dict values are ints 0-127,
+    keys are: vibrato, volume, pan, expression, sustain, reverb, chorus.
+    """
+
+    values = {}
+    instrs = []
+
+    def frameReceived(self, frame):
+        ValuesHandler.frameReceived(self, frame)
+
+        print frame
+        kwargs = {}
+        k, v = frame.split('-')
+        kwargs[k] = int(v)
+
+        for i in self.instrs:
+            i.controlChange(**kwargs)
+
+
+
 
 import os.path
 HERE = os.path.dirname(__file__)
@@ -100,8 +114,8 @@ def start():
     clock = getClock()
     root = File(HERE)
     site = WebSocketSite(root)
-    site.addHandler('/test', ArpHandler)
-    site.addHandler('/slider', SliderHandler)
+    site.addHandler('/list', ArpHandler)
+    site.addHandler('/cc', ControlChangeHandler)
     reactor.listenTCP(8347, site)
 
 
