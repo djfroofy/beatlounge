52dc826ce93f7fd65c11917c282f9f81c6973bc9
Drew Smathers <drew.smathers@gmail.com>
2011-05-25 23:07:49 -0400

    SwitcherElement/Switchers

--- bl/ue/web/api.py
+++ bl/ue/web/api.py
@@ -80,37 +80,85 @@ class ArpElement(Element):
         self.powerup()
 
     def powerup(self):
-        self.arp = getattr(arp, self.type)(self.values)
+        self.arp = self.noteFactory = getattr(arp, self.type)(self.values)
 
     def update(self, state):
         Element.update(self, state)
         self.arp.reset(self.values)
         log.msg('%s reset arp with values: %s' % (self, self.values))
 
+
 class Arps(Collection):
     defaultElementClass = ArpElement
     exposedElementAttributes = 'name', 'type'
 
 
+class SwitcherElement(Element):
+    updatableAttributes = 'switchee_uri', 'values', 'amount', 'octaves', 'oscillate'
+    exposedAttributes = 'name', 'type', 'switchee_uri', 'values', 'amount', 'octaves', 'oscillate'
+    switcherClasses = 'OctaveArp', 'Adder',
+    switchers = None
+    arps = None
+
+    def __init__(self, name=None, type=None, switchee_uri=None, values=(),
+                 amount=0, octaves=2, oscillate=False):
+        self.name = name
+        self.type = type
+        self.switchee_uri = switchee_uri
+        self.values = values
+        self.amount = amount
+        self.octaves = octaves
+        self.oscillate = oscillate
+        self.powerup()
+
+    def powerup(self, update=False):
+        c, name = self.switchee_uri.split('/')
+        collection = getattr(self, c)
+        switchee = collection[name].noteFactory
+        if not update:
+            switcherClass = getattr(arp, self.type)
+            self.switcher = self.noteFactory = switcherClass(switchee, self.values)
+        else:
+            if self.values != self.switcher.values:
+                self.switcher.reset(self.values)
+            if switchee != self.switcher.arp:
+                self.switcher.switch(switchee)
+        if hasattr(self.switcher, 'amount') and self.amount != self.switcher.amount:
+            self.switcher.amount = self.amount
+        if hasattr(self.switcher, 'octaves') and self.octaves != self.switcher.octaves:
+            self.switcher.octaves = self.octaves
+        if hasattr(self.switcher, 'oscillate') and self.oscillate != self.switcher.oscillate:
+            self.switcher.oscillate = self.oscillate
+
+    def update(self, state):
+        Element.update(self, state)
+        self.powerup(True)
+
+
+class Switchers(Collection):
+    defaultElementClass = SwitcherElement
+    exposedElementAttributes = 'name', 'type', 'switchee_uri'
+
+
 class PlayerElement(Element):
-    exposedAttributes = ('name', 'type', 'instrument_name', 'note_arp_name', 'clock_name', 'velocity_arp_name',
+    exposedAttributes = ('name', 'type', 'instrument_name', 'note_factory_uri', 'clock_name', 'velocity_arp_name',
                          'sustain', 'interval', 'playing')
-    updatableAttributes = ('interval', 'playing', 'instrument_name', 'note_arp_name', 'velocity_arp_name',
+    updatableAttributes = ('interval', 'playing', 'instrument_name', 'note_factory_uri', 'velocity_arp_name',
                           'sustain')
     allowedInstrumenTypes = 'NotePlayer',
 
-
     arps = None
+    switchers = None
     instruments = None
     clocks = None
 
-    def __init__(self, name=None, type='NotePlayer', instrument_name=None, note_arp_name=None,
+    def __init__(self, name=None, type='NotePlayer', instrument_name=None, note_factory_uri=None,
                  velocity_arp_name=None, clock_name='default', sustain=None,
                  interval=None, playing=False):
         self.name = name
         self.type = type
         self.instrument_name = instrument_name
-        self.note_arp_name = note_arp_name
+        self.note_factory_uri = note_factory_uri
         self.velocity_arp_name = velocity_arp_name
         self.clock_name = clock_name
         self.interval = interval
@@ -120,7 +168,9 @@ class PlayerElement(Element):
 
     def powerup(self):
         instrument = self.instruments[self.instrument_name].instrument
-        note_arp = self.arps[self.note_arp_name].arp
+        c, name = self.note_factory_uri.split('/')
+        coll = getattr(self, c)
+        note_arp = coll[name].noteFactory
         velocity_arp = self.arps[self.velocity_arp_name].arp
         clock = self.clocks[self.clock_name].clock
         if self.type not in self.allowedInstrumenTypes:
@@ -138,8 +188,8 @@ class PlayerElement(Element):
         if 'instrument_name' in state:
             instrument = self.instruments[self.instrument_name].instrument
             self.player.instr = instrument
-        if 'note_arp_name' in state:
-            note_arp = self.arps[self.note_arp_name].arp
+        if 'note_factory_uri' in state:
+            note_arp = self.arps[self.note_factory_uri].arp
             self.player.noteFactory = note_arp
         if 'velocity_arp_name' in state:
             velocity_arp = self.arps[self.velocity_arp_name].arp
@@ -180,14 +230,22 @@ def apiSite(sfdir):
     players = Players()
     playerResource = IResource(players)
 
+    switchers = Switchers()
+    switcherResource = IResource(switchers)
+
     PlayerElement.arps = arps
+    PlayerElement.switchers = switchers
     PlayerElement.instruments = instruments
     PlayerElement.clocks = clocks
+
+    SwitcherElement.arps = arps
+    SwitcherElement.switchers = switchers
 
     root = Resource()
     root.putChild('clocks', clockResource)
     root.putChild('instruments', instrumentResource)
     root.putChild('arps', arpResource)
+    root.putChild('switchers', switcherResource)
     root.putChild('players', playerResource)
 
     site = Site(root)
