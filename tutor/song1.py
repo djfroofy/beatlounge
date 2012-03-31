c80737fe5ba15e9f5968f6e7f812abb74f30085b
dsmathers <drew.smathers@transgaming.com>
2012-03-31 13:34:00 -0400

    make song1 have more fun things to do - random harmonized scales

--- tutor/song1.py
+++ tutor/song1.py
@@ -30,28 +30,72 @@ SF2DIR = os.path.join(os.path.dirname(__file__), 'sf2')
 instrument = Instrument(os.path.join(SF2DIR, 'piano.sf2'))
 
 
+# Let's define some musical intervals in terms for virtual clock ticks
+# Meter.divisionToTicks() API. Note that dtt() is a shorthand alias for
+# divisionToTicks()
+
+meter = clock.meter
+quaver = meter.dtt(1,8)
+quarter = meter.dtt(1,4)
+half = meter.dtt(1,2)
+one_and_half = meter.dtt(3,2)
+scale = range(36, 84)
+
 def hitsomenote():
 
 
     # We have no clue what to do with this piano. Let's just
     # hit a note with some random amount of force.
 
-    note = random.randint(36, 84)
+    note = random.choice(scale)
     velocity = random.randint(40, 127)
     instrument.playnote(note, velocity)
 
 
     # And we'll take our finger off ... some time later
-
-    release = random.choice((0.125, 0.25, 0.5, 1, 1.5))
-    clock.callAfterMeasures(release, instrument.stopnote, note)
+    release = random.choice((quaver, quarter, half, one_and_half))
+    clock.callLater(release, instrument.stopnote, note)
 
 
 event = clock.schedule(hitsomenote)
-event.startLater(1, 0.125)
+event.startAfter(1, (1,8))
 
 
 # Now try this exercise from the shell.
 #
 # >>> event.stopLater(1) # Stop on the next measure
 # >>> event.startLater(1, 0.0625) # restart, but this time every semiquaver
+
+# Also try redfining scale to something less ... random.
+# Here's another example to play with
+
+def harmonize(steps):
+    current = 0
+    for step in steps:
+        current += step
+    return steps + [ divmod(current, 12)[1] ]
+
+random_steps = [
+        random.choice([2,2,3,4,4,4,4]), random.choice([2,2,4,3,3,3,3]),
+        random.choice([3,4,2,2,2,2,2]), random.choice([4,3,1,2,2,2,2])]
+random_steps = harmonize(random_steps)
+
+random_start = random.randint(36,48)
+
+def use_scale(steps, start, repeat=2):
+    global scale
+    print 'using steps', steps
+    print 'using start', start
+    new_scale = [start]
+    for _ in range(repeat):
+        for step in steps:
+            new_scale.append(new_scale[-1] + step)
+    print 'new scale', new_scale
+    scale = new_scale
+
+# Now call use_scale(random_steps, random_start)
+#
+# Then try playing some with your own values instead of random_steps
+# and random_start.
+
+
