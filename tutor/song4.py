e22c6e4f62e2392f2d2ea7101c41a31a8eedc9b3
dsmathers <drew.smathers@transgaming.com>
2012-04-01 14:14:28 -0400

    added song4

---
+++ tutor/song4.py
@@ -0,0 +1,45 @@
+# Yeah, we learn about arpegiators ...
+# And then rudiments
+
+from itertools import cycle
+import random
+
+from bl.arp import OrderedArp, OctaveArp, Adder
+from bl.player import Player
+from bl.instrument.fsynth import Layer
+from bl.scheduler import clock
+from bl.rudiments import FiveStrokeRoll, RudimentSchedulePlayer
+
+from tutor.complib import piano_f, kit_f
+
+
+dtt = clock.meter.dtt
+
+notes = Adder(OctaveArp(OrderedArp([60, 48, 62, 64, 69, 71, 46])))
+notes.arp.octaves = 2
+notes.arp.oscillate = True
+notes.amount = 12
+
+velocity = Adder(OrderedArp([120,80,89,83,120,120,80,79]))
+
+piano = piano_f()
+pianoPlayer = Player(piano, notes, velocity, stop=lambda: random.randint(12,60),
+                    interval=dtt(1,16))
+pianoPlayer.startPlaying()
+
+
+kit = kit_f()
+drumRudiment = FiveStrokeRoll()
+drumPlayer = RudimentSchedulePlayer(kit, drumRudiment, 51, 48)
+drumPlayer.startPlaying()
+
+# Now we see how we can set up a simple "process" to change the key periodically
+
+keys = cycle([-18] * 8 + [-18,-37,-18,-37] * 4 + [-18,-26,-18,-26,-37,-26,-18])
+def change_key():
+    notes.amount = keys.next()
+change_key_event = clock.schedule(change_key)
+change_key_event.startAfter((1,1), (1,1))
+
+
+# TODO say some things with some enthusiasm ... late
