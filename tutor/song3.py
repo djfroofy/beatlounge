17e697daa51b1f84bbf177b37b9d37ebda02735f
dsmathers <drew.smathers@transgaming.com>
2012-04-12 08:17:33 -0400

    updates to song3

--- tutor/song3.py
+++ tutor/song3.py
@@ -13,27 +13,33 @@ from tutor.complib import bass_f, kit_f
 
 dtt = clock.meter.dtt
 
-notes = Adder(RandomArp())
-notes.reset([60, 60, 62, 60, 60, 60, 60, 61, 48, 50, 60, 60, 60, 60, 60, 71, 60,
-             60,60,60,60,60,60,50])
-notes.amount = -18
-
-velocity = Adder(OrderedArp([120,80,89,83,120,120,80,79]))
-
+notes1 = Adder(RandomArp())
+notes1.reset([60, 60, 62, 60, 60, 60, 60, 61, 48, 50, 60, 60, 60, 60, 60, 71,
+              60, 60,60,60,60,60,60,50])
+notes1.amount = -18
+velocity1 = Adder(OrderedArp([120,80,89,83,120,120,80,79]))
 bass = bass_f()
-kit = kit_f()
-layer = Layer([bass, kit])
-
-player = Player(layer, notes, velocity, stop=lambda: random.randint(12,60),
+bass_player = Player(bass, notes1, velocity1,
+                stop=lambda: random.randint(12,60),
                 interval=dtt(1,16))
-player.startPlaying()
+bass_player.startPlaying()
+
+
+notes2 = Adder(RandomArp())
+notes2.reset([60, 60, 62])
+notes2.amount = -18
+velocity2 = Adder(OrderedArp([120,80,120,83,120,110,90,100]))
+kit = kit_f()
+kit_player = Player(kit, notes2, velocity2, stop=lambda: random.randint(12,60),
+                interval=dtt(1,16))
+kit_player.startPlaying()
 
 
 # Now we see how we can set up a simple "process" to change the key periodically
 
 keys = cycle([-18] * 8 + [-18,-37,-18,-37] * 4 + [-18,-26,-18,-26,-37,-26,-18])
 def change_key():
-    notes.amount = keys.next()
+    notes1.amount = keys.next()
 change_key_event = clock.schedule(change_key)
 change_key_event.startAfter((1,1), (1,1))
 
